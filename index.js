0915693350840d5f28eb2ffece17b455794b718c
Myles Reyburn <mylesreyburn@gmail.com>
2023-01-31 17:12:41 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let num3 = 9;
+let num4 = 22;
+
+console.log(num1, num2);
+
+let multiply = (num1 * num2);
+
+let random = Math.floor(Math.random() * 10);
+
+console.log(`Random: ${random}`);
+
+let mod = (num4 % num3);
+
+console.log(`mod: ${mod}`)
+
+let max = Math.max(20, 2, 3);
